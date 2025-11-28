2f85dd6b3a66255eb07937d010d439bede69fe60
BasketBandit <errorsfound@hotmail.co.jp>
2025-11-28 13:25:59 +0000

    implemented the remaining 4-8 decks stand soft 17 ruleset

--- src/main/java/com/basketbandit/player/AdvantagePlayer.java
+++ src/main/java/com/basketbandit/player/AdvantagePlayer.java
@@ -11,11 +11,138 @@ public class AdvantagePlayer extends Player {
 
     // basic strategy
     public Action decideAction(Card dealerCard) {
-        // hard
-        int value = dealerCard.value() == 1 ? 11 : dealerCard.value(); // deal with ace
-        switch(value) {
+        int playerHandValue = hand().value();
+        int playerFirstCardValue = hand().cards().getFirst().value();
+        int dealerHandValue = dealerCard.value() == 1 ? 11 : dealerCard.value(); // deal with ace
+
+        // split
+        if(hand().cards().size() == 2 && hand().isPair()) {
+            switch(dealerHandValue) {
+                case 2, 3, 4 -> {
+                    switch(playerFirstCardValue) {
+                        case 2, 3, 6, 7, 8, 9, 1 -> {
+                            return Action.SPLIT;
+                        }
+                        case 4 -> {
+                            return Action.HIT;
+                        }
+                    }
+                }
+                case 5, 6 -> {
+                    switch(playerFirstCardValue) {
+                        case 2, 3, 4, 6, 7, 8, 9, 1 -> {
+                            return Action.SPLIT;
+                        }
+                    }
+                }
+                case 7 -> {
+                    switch(playerFirstCardValue) {
+                        case 2, 3, 7, 8, 1 -> {
+                            return Action.SPLIT;
+                        }
+                        case 4, 6 -> {
+                            return Action.HIT;
+                        }
+                        case 9 -> {
+                            return Action.STAND;
+                        }
+                    }
+                }
+                case 8, 9 -> {
+                    switch(playerFirstCardValue) {
+                        case 8, 9, 1 -> {
+                            return Action.SPLIT;
+                        }
+                        case 2, 3, 4, 6, 7 -> {
+                            return Action.HIT;
+                        }
+                    }
+                }
+                case 10, 1 -> {
+                    switch(playerFirstCardValue) {
+                        case 8, 1 -> {
+                            return Action.SPLIT;
+                        }
+                        case 2, 3, 4, 6, 7 -> {
+                            return Action.HIT;
+                        }
+                        case 9 -> {
+                            return Action.STAND;
+                        }
+                    }
+                }
+            }
+        }
+
+        // soft, non-split
+        if(hand().isSoft()) {
+            switch(dealerHandValue) {
+                case 2, 7, 8 -> {
+                    switch(playerHandValue) {
+                        case 13, 14, 15, 16, 17 -> {
+                            return Action.HIT;
+                        }
+                        default -> {
+                            return Action.STAND;
+                        }
+                    }
+                }
+                case 3 -> {
+                    switch(playerHandValue) {
+                        case 13, 14, 15, 16 -> {
+                            return Action.HIT;
+                        }
+                        case 17, 18 -> {
+                            return Action.DOUBLE;
+                        }
+                        default -> {
+                            return Action.STAND;
+                        }
+                    }
+                }
+                case 4 -> {
+                    switch(playerHandValue) {
+                        case 13, 14 -> {
+                            return Action.HIT;
+                        }
+                        case 15, 16, 17, 18 -> {
+                            return Action.DOUBLE;
+                        }
+                        default -> {
+                            return Action.STAND;
+                        }
+                    }
+                }
+                case 5, 6 -> {
+                    switch(playerHandValue) {
+                        case 13, 14, 15, 16, 17, 18 -> {
+                            return Action.DOUBLE;
+                        }
+                        default -> {
+                            return Action.STAND;
+                        }
+                    }
+                }
+                case 9, 10, 1 -> {
+                    switch(playerHandValue) {
+                        case 13, 14, 15, 16, 17, 18 -> {
+                            return Action.HIT;
+                        }
+                        default -> {
+                            return Action.STAND;
+                        }
+                    }
+                }
+                default -> {
+                    return Action.STAND;
+                }
+            }
+        }
+
+        // hard, non-split
+        switch(dealerHandValue) {
             case 2 -> {
-                switch(hand().value()) {
+                switch(playerHandValue) {
                     case 4, 5, 6, 7, 8, 9, 12 -> {
                         return Action.HIT;
                     }
@@ -28,7 +155,7 @@ public class AdvantagePlayer extends Player {
                 }
             }
             case 3 -> {
-                switch(hand().value()) {
+                switch(playerHandValue) {
                     case 4, 5, 6, 7, 8, 12 -> {
                         return Action.HIT;
                     }
@@ -41,7 +168,7 @@ public class AdvantagePlayer extends Player {
                 }
             }
             case 4, 5, 6 -> {
-                switch(hand().value()) {
+                switch(playerHandValue) {
                     case 4, 5, 6, 7, 8 -> {
                         return Action.HIT;
                     }
@@ -54,7 +181,7 @@ public class AdvantagePlayer extends Player {
                 }
             }
             case 7, 8, 9, 1 -> {
-                switch(hand().value()) {
+                switch(playerHandValue) {
                     case 4, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16 -> {
                         return Action.HIT;
                     }
@@ -67,7 +194,7 @@ public class AdvantagePlayer extends Player {
                 }
             }
             case 10 -> {
-                switch(hand().value()) {
+                switch(playerHandValue) {
                     case 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16 -> {
                         return Action.HIT;
                     }
